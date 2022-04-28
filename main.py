515e02e78d13a1959a185f991aa34290d6577511
natosafix <92174064+natosafix@users.noreply.github.com>
2022-04-28 19:19:41 +0500

    Save/Open File

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
-# This is a sample Python script.
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtGui import QFont, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMenu, \
+    QMenuBar
 
 
-# Press the green button in the gutter to run the script.
+class Redactor(QMainWindow):
+    _WINDOW_WIDTH = 1000
+    _WINDOW_HEIGHT = 800
+
+    def __init__(self):
+        super(Redactor, self).__init__()
+
+        desktop = QApplication.desktop()
+        x = desktop.width()
+        y = desktop.height()
+
+        self.setWindowTitle("Текстовый редактор")
+        self.setGeometry(x // 2 - Redactor._WINDOW_WIDTH // 2,
+                         y // 2 - Redactor._WINDOW_HEIGHT // 2,
+                         Redactor._WINDOW_WIDTH,
+                         Redactor._WINDOW_HEIGHT)
+
+        self.text_edit = QtWidgets.QTextEdit(self)
+
+        self.setCentralWidget(self.text_edit)
+        self.font = QFont()  # создаём объект шрифта
+        self.font.setFamily("Rubik")  # название шрифта
+        self.font.setPointSize(20)  # размер шрифта
+        self.font.setUnderline(True)  # подчёркивание
+
+        self.text_edit.setFont(self.font)
+        text_color = QColor(255, 0, 0, 255)
+        background_color = QColor()
+        background_color.setRgb(0, 255, 0, 255)
+        self.text_edit.setTextColor(text_color)
+        self.text_edit.setTextBackgroundColor(background_color)
+        self.create_menu_bar()
+
+    def create_menu_bar(self):
+        self.menu_bar = QMenuBar(self)
+        self.setMenuBar(self.menu_bar)
+        file_menu = QMenu("Файл", self)
+        self.menu_bar.addMenu(file_menu)
+        file_menu.addAction("Открыть", self.action_clicked)
+        file_menu.addAction("Сохранить", self.action_clicked)
+
+    @QtCore.pyqtSlot()
+    def action_clicked(self):
+        action = self.sender()
+        action_text = action.text()
+        print(action_text)
+        if action_text == "Открыть":
+            file_name = QFileDialog.getOpenFileName()[0]
+            try:
+                with open(file_name, 'r') as f:
+                    data = f.read()
+                    self.text_edit.setText(data)
+            except FileNotFoundError:
+                print("Such file does not exist")
+        elif action_text == "Сохранить":
+            file_name = QFileDialog.getSaveFileName()[0]
+            try:
+                with open(file_name, 'w') as f:
+                    data = self.text_edit.toPlainText()
+                    f.write(data)
+            except FileNotFoundError:
+                print("Such file does not exist")
+
+
+def initialise_window():
+    app = QApplication(sys.argv)
+    redactor = Redactor()
+
+    redactor.show()
+    sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    initialise_window()
